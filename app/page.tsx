cb673b38f6abdb251ae7a9c2ac5ae2344f221888
Dhruvi2416 <dhruvikalpesh2001@gmail.com>
2026-04-08 16:42:58 +0530

    intro to shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <div>
       <h1>Hello Next.js!</h1>
-      <Button className="border">Click Me</Button>
-       <Button variant="outline">Click Me</Button>
+      {/* <Button className="border">Click Me</Button> */}
+       <Button variant="outline" size={"lg"}>Click Me</Button>
         <Button variant="secondary">Click Me</Button>
          <Button variant="default" className="secondary">Click Me</Button>
           <Button variant="destructive">Click Me</Button>
